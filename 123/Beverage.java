e8222fe77300a8fe3fc7ccd71af08bd2f4c21876
Henn <HennOgyrchik@gmail.com>
2023-07-21 13:47:18 +0300

    delete 123

--- 123/Beverage.java
+++
@@ -1,7 +0,0 @@
-import java.util.Calendar;
-
-public class Beverage extends Product{
-    public Beverage  (String name, int price, int quantity, Calendar bestBefore){
-        super(name,price,quantity,bestBefore);
-    }
-}
